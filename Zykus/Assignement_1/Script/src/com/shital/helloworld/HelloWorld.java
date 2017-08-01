d086e0496dd6c1ff9e0c4878479634942297c551
ShitalRindhe <30631454+ShitalRindhe@users.noreply.github.com>
2017-08-01 09:59:54 -0700

    Add files via upload
    Please refer "Reference_for_user.txt" file to find the details of assignment files.
    Main Files are as follow:
    Build.xml

---
+++ Zykus/Assignement_1/Script/src/com/shital/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.shital.helloworld;
+
+/* HelloWorld.java
+ */
+ 
+public class HelloWorld
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hi Shital!");
+    }
+}
